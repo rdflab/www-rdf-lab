c5fb232c760b22ca35e234896f26d04241a96fd4
Antony Holmes <antony.b.holmes@gmail.com>
2019-11-23 15:45:52 -0500

    Bug fixes and updates.

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import "../styles/global.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -35,6 +36,7 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
+        <button class="button is-primary">Primary</button>
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
