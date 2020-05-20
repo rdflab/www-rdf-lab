0d78058adbbf3b015a7d7e1b766c333b2457fe93
Antony B Holmes <antony.b.holmes@gmail.com>
2020-05-20 15:01:25 -0400

    Bug fixes and updates.

--- src/components/layout.js
+++ src/components/layout.js
@@ -38,9 +38,17 @@ const Layout = ({
     <>
       <Helmet>
         <link
-          href="https://fonts.googleapis.com/css?family=Orbitron:600&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap"
           rel="stylesheet"
         />
+        {/* <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600;700&display=swap"
+          rel="stylesheet"
+        /> */}
       </Helmet>
 
       {title !== "" && <SEO title={title} />}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   theme: {
     fontFamily: {
-      sans: ["Lato"],
+      sans: ["Inter"],
     },
     extend: {
       width: {
